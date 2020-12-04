5a5a9fafdca7e33594ce6507e82ea4cd9014720a
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-03 19:31:31 -0500

    updated site

--- public/js/guild-counter.js
+++ public/js/guild-counter.js
@@ -1,13 +1,17 @@
-$( document ).ready(function() {
-    $.getJSON( "https://galactus.automute.us/", function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          items.push( "<li id='" + key + "'>" + val + "</li>" );
-        });
-       
-        $( "<ul/>", {
-          "class": "my-new-list",
-          html: items.join( "" )
-        }).appendTo( "body" );
-      });
+
+$.getJSON('https://stats.automute.us/stats/api', function (data) {
+    var guilds = `${data.totalGuilds}`
+    var activeGames = `${data.activeGames}`
+    $("#guilds").html(guilds);
+    $("#activeGames").html(activeGames);
+  });
+
+setInterval(function(){ 
+  //this code runs every 5 seconds
+$.getJSON('https://stats.automute.us/stats/api', function (data) {
+    var guilds = `${data.totalGuilds}`
+    var activeGames = `${data.activeGames}`
+    $("#guilds").html(guilds);
+    $("#activeGames").html(activeGames);
 });
+}, 5000);
