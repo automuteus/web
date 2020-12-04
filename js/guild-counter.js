f5990f9ddfa11283f0c1f37af451f16ebc473892
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-03 20:12:16 -0500

    updates every 10 seconds now

--- js/guild-counter.js
+++ js/guild-counter.js
@@ -7,11 +7,11 @@ $.getJSON('https://stats.automute.us/stats/api', function (data) {
   });
 
 setInterval(function(){ 
-  //this code runs every 5 seconds
+  //this code runs every 10 seconds
 $.getJSON('https://stats.automute.us/stats/api', function (data) {
     var guilds = `${data.totalGuilds}`
     var activeGames = `${data.activeGames}`
     $("#guilds").html(guilds);
     $("#activeGames").html(activeGames);
 });
-}, 5000);
+}, 10000);
