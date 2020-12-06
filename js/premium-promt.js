8caa3fe8a624ef9d4135d7987fa66733de7855f8
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-05 23:54:26 -0500

    ?guild=1234567890 is a thing now

--- js/premium-promt.js
+++ js/premium-promt.js
@@ -5,7 +5,6 @@ const guild = urlParams.get('guild')
 function ask(button_id) {
     if (urlParams.get('guild') != null) {
         if (guild == "754465589958803548") return alert("You can't use example ID");
-        alert("You will now be redirected to PayPal")
         var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${urlParams.get('guild')}`;
         window.location.href = '' + loc + '';
     } else {
