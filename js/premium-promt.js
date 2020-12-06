f0b555d4c28de2757d0ee678f2fd974ce93be0c4
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-06 00:23:23 -0500

    ?guild=1234567890 is a thing now

--- js/premium-promt.js
+++ js/premium-promt.js
@@ -4,17 +4,19 @@ const guild = urlParams.get('guild')
 
 function ask(button_id) {
     if (urlParams.get('guild') != null) {
-        if (guild == "754465589958803548") return alert("You can't use example ID");
-        var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${urlParams.get('guild')}`;
+        if (urlParams.get('guild') == null || urlParams.get('guild') == "") return window.location.href = '/premium.html';
+        if (urlParams.get('guild').length < 17 || urlParams.get('guild').length > 20) return window.location.href = '/premium.html';
+        if (isNaN(urlParams.get('guild'))) return  alert("Invalid ID. You can get your Server ID with developer tools in discord.") & (window.location.href = '/premium.html'); 
+        if (urlParams.get('guild') == "754465589958803548") return alert("Thats the AutoMuteUs server you silly goose!");
+        var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${guild}`;
         window.location.href = '' + loc + '';
     } else {
-    var guild = prompt("Please provide the Server ID you want premium for:", "754465589958803548");
-    if (guild == null || guild == "") return alert("This feild can not be blank.");
-    if (guild.length < 17 || guild.length > 20) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
-    if (isNaN(guild)) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
-    if (guild == "754465589958803548") return alert("You can't use example ID");
-    alert("You will now be redirected to PayPal")
-    var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${guild}`;
-    window.location.href = '' + loc + '';
+        var guild = prompt("Please provide the Server ID you want premium for:", "754465589958803548");
+        if (guild == null || guild == "") return alert("This feild can not be blank.");
+        if (guild.length < 17 || guild.length > 20) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
+        if (isNaN(guild)) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
+        if (guild == "754465589958803548") return alert("You can't use example ID");
+        var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${guild}`;
+        window.location.href = '' + loc + '';
     }
 }
