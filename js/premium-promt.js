f7f2960ee6c27c542be1205cac02285bea9a1739
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-05 22:19:16 -0500

    pay for premium works well :ok_handsign:

--- js/premium-promt.js
+++ js/premium-promt.js
@@ -1,17 +1,10 @@
-function askGuildId() {
-var guild = prompt("Please provide the Server ID you want premium for:", "754465589958803548");
-    if (guild == null || guild == "") {
-      alert("Invalid ID")
-    } else {
-      if(isNaN(guild)){
-          alert("Please make sure you have the correct Server ID.");
-      } else {
-          if (guild=="754465589958803548"){
-              alert("You can't have example ID");
-          } else {
-              var loc = "https://crewmate.xyz/donate/amu?guildid="+guild; 
-              window.location.href = '' + loc + ''; 
-          }
-      }
-    }
+function ask(button_id) {
+    var guild = prompt("Please provide the Server ID you want premium for:", "754465589958803548");
+    if (guild == null || guild == "") return alert("This feild can not be blank.");
+    if (guild.length < 17 || guild.length > 20) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
+    if (isNaN(guild)) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
+    if (guild == "754465589958803548") return alert("You can't use example ID");
+    alert("You will now be redirected to PayPal")
+    var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${guild}`;
+    window.location.href = '' + loc + '';
 }
