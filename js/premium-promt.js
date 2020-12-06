2b89b720cbeb475d7b70285daa564243c5fa59ad
Aiden <58155937+Wolfhound905@users.noreply.github.com>
2020-12-05 23:50:30 -0500

    ?guild=1234567890 is a thing now

--- js/premium-promt.js
+++ js/premium-promt.js
@@ -1,4 +1,14 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const guild = urlParams.get('guild')
+
 function ask(button_id) {
+    if (urlParams.get('guild') != null) {
+        if (guild == "754465589958803548") return alert("You can't use example ID");
+        alert("You will now be redirected to PayPal")
+        var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${urlParams.get('guild')}`;
+        window.location.href = '' + loc + '';
+    } else {
     var guild = prompt("Please provide the Server ID you want premium for:", "754465589958803548");
     if (guild == null || guild == "") return alert("This feild can not be blank.");
     if (guild.length < 17 || guild.length > 20) return alert("Invalid ID. You can get your Server ID with developer tools in discord.");
@@ -7,4 +17,5 @@ function ask(button_id) {
     alert("You will now be redirected to PayPal")
     var loc = `https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=${button_id}&custom=${guild}`;
     window.location.href = '' + loc + '';
+    }
 }
