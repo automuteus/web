b0340a288cdf3c718a05fcfaa6eba252b3bb5630
Neil Sparks <yoshirahh@users.noreply.github.com>
2021-01-06 22:33:39 -0700

    Revert "Discord auth and premium page revamp"

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import { AnimatePresence } from "framer-motion";
-import { Provider } from "next-auth/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "reflect-metadata";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // Import the CSS
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 import "../styles/global.css";
 
 export default function App({ Component, pageProps, router }) {
   return (
-    <Provider session={pageProps.session}>
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </Provider>
+    <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route} />
+    </AnimatePresence>
   );
 }
