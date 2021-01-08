6f1c1da68f40c8eb7d635061380e065348c4a076
Neil Sparks <yoshirahh@users.noreply.github.com>
2021-01-08 13:58:54 -0700

    Revert "Main Deploy"

--- components/layout.js
+++ components/layout.js
@@ -4,72 +4,26 @@ import { motion } from "framer-motion";
 
 import Header from "./header";
 import Footer from "./footer";
+import styles from "./layout.module.css";
 
-export default function Layout(props) {
+export default function Layout({ children, home }) {
   return (
-    <Container fluid className={`main_container ${props.className}`}>
+    <Container fluid className={styles.main_container}>
       <Head>
-        {/* HTML Meta Tags */}
+        <link rel="icon" href="/favicon.ico" />
         <title>AutoMuteUs</title>
-        <meta
-          name="description"
-          content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
-        />
-        <meta name="theme-color" content="#7289DA" />
-
-        {/* Google / Search Engine Tags */}
-        <meta itemProp="name" content="AutoMuteUs" />
-        <meta
-          itemProp="description"
-          content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
-        />
-        <meta
-          itemProp="image"
-          content={`${
-            process.env.NEXTAUTH_URL
-          }/public/assets/img/logo_embed.png`}
-        />
-
-        {/* Discord/Facebook Meta Tags */}
-        <meta property="og:url" content={process.env.NEXTAUTH_URL} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="AutoMuteUs" />
-        <meta
-          property="og:description"
-          content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
-        />
-        <meta
-          property="og:image"
-          content={`${
-            process.env.NEXTAUTH_URL
-          }/public/assets/img/logo_embed.png`}
-        />
-
-        {/* Twitter Meta Tags */}
-        <meta name="twitter:title" content="AutoMuteUs" />
-        <meta
-          name="twitter:description"
-          content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
-        />
-        <meta
-          name="twitter:image"
-          content={`${
-            process.env.NEXTAUTH_URL
-          }/public/assets/img/logo_embed.png`}
-        />
       </Head>
-
       <Header />
       <motion.main
         exit={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
         id="main-content"
-        className={`d-flex p-3 ${props.innerClassName}`}
+        className=" d-flex flex-lg-row flex-column align-items-center p-3"
       >
-        {props.children}
+        {children}
       </motion.main>
-      <Footer effect={props.effect} effectActive={props.effectActive} />
+      <Footer />
     </Container>
   );
 }
