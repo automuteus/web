30b204acb3ba5bc8cd3f69f1c26c48b3459fd882
Neil Sparks <neil.sparks.95@gmail.com>
2021-01-10 16:05:28 -0700

    Rearrange navbar to highlight user/dashboard access

--- components/header.js
+++ components/header.js
@@ -1,18 +1,28 @@
 import React from "react";
-import Image from "next/image";
+import { useRouter } from "next/router";
 
-import { Navbar, Nav, Badge } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDiscord, faGithub } from "@fortawesome/free-brands-svg-icons";
+import {
+  faDiscord,
+  faGithub,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 import Link from "next/link";
 
 import { signIn, signOut, useSession } from "next-auth/client";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCheckCircle,
+  faCode,
+  faHome,
+  faTachometerAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./header.module.css";
 
 const site_logo = "/assets/img/logo_2021.png";
 
 export default function Header() {
+  const router = useRouter();
   const [session, loading] = useSession();
 
   return (
@@ -23,28 +33,89 @@ export default function Header() {
       className={`${styles.header}`}
     >
       <Navbar.Toggle aria-controls="primary-navbar-nav" />
-      <Navbar.Brand>
-        <Link href="/" passHref>
-          <a>
-            <img
-              src={site_logo}
-              className="d-inline-block align-top"
-              alt="AutoMuteUs"
-              width="50"
-              height="50"
-            />
-          </a>
-        </Link>
+      <Navbar.Brand className="d-none d-lg-inline-block">
+        <img
+          src={site_logo}
+          className="align-top"
+          alt="AutoMuteUs"
+          width="50"
+          height="50"
+        />
       </Navbar.Brand>
-      <Navbar.Collapse id="primary-navbar-nav">
+      <Nav className="align-items-center flex-row order-lg-2">
+        {loading && <Navbar.Text>Checking login...</Navbar.Text>}
+        {!loading && !session && (
+          <Nav.Link onClick={() => signIn("discord")}>
+            <FontAwesomeIcon icon={faDiscord} size="lg" className="mr-2" />
+            <span className="d-none d-sm-block">Sign In</span>
+          </Nav.Link>
+        )}
+        {!loading && session && (
+          <>
+            {process.env.NODE_ENV === "development" && (
+              <>
+                <Link href="/dashboard" passHref>
+                  <Nav.Link>
+                    <span className="d-block d-sm-none mr-1">
+                      <FontAwesomeIcon
+                        size="lg"
+                        icon={faTachometerAlt}
+                        className=""
+                      />
+                    </span>
+                    <span className="d-none d-sm-block mr-1">Dashboard </span>
+                  </Nav.Link>
+                </Link>
+              </>
+            )}
+            <Navbar.Text as="div" className="user-logged-in">
+              <img
+                src={
+                  session.user.image !== ""
+                    ? session.user.image
+                    : "https://upload.wikimedia.org/wikipedia/commons/9/90/Discord-512.webp"
+                }
+                alt={session.user.name}
+                className="user-image mr-2"
+              />
+              <div className="user-details d-none d-sm-block">
+                <div className="user-name">
+                  {session.user.name}
+                  {session.user.verified && (
+                    <FontAwesomeIcon className="ml-2" icon={faCheckCircle} />
+                  )}
+                </div>
+                <small className="user-signout" onClick={signOut}>
+                  Sign Out
+                </small>
+              </div>
+            </Navbar.Text>
+          </>
+        )}
+      </Nav>
+
+      <Navbar.Collapse id="primary-navbar-nav order-lg-1">
         <Nav className="mr-auto">
+          {router.pathname !== "/" && (
+            <Link href="/" passHref>
+              <Nav.Link>
+                <FontAwesomeIcon icon={faHome} size="lg" className="mr-2" />
+                Home
+              </Nav.Link>
+            </Link>
+          )}
           <Nav.Link href="https://discord.gg/vwWXs8Z" target="_blank">
-            Discord Support Server
+            <FontAwesomeIcon icon={faDiscord} size="lg" className="mr-2" />
+            Support Server
           </Nav.Link>
           <Nav.Link href="https://github.com/denverquane/automuteus#commands">
+            <FontAwesomeIcon icon={faCode} size="lg" className="mr-2" />
             Commands
           </Nav.Link>
-          <Nav.Link href="https://youtu.be/kO4cqMKV2yI">Tutorials</Nav.Link>
+          <Nav.Link href="https://youtu.be/kO4cqMKV2yI">
+            <FontAwesomeIcon icon={faYoutube} size="lg" className="mr-2" />
+            Tutorials
+          </Nav.Link>
           <Nav.Link
             href="https://github.com/denverquane/automuteus"
             target="_blank"
@@ -52,54 +123,6 @@ export default function Header() {
             <FontAwesomeIcon icon={faGithub} size="lg" className="mr-2" />
             GitHub
           </Nav.Link>
-        </Nav>
-        <hr className="d-lg-none border border-white" />
-        <Nav className="align-items-lg-center">
-          {loading && <Navbar.Text>Checking login...</Navbar.Text>}
-          {!loading && !session && (
-            <Nav.Link onClick={() => signIn("discord")}>
-              <FontAwesomeIcon icon={faDiscord} size="lg" className="mr-2" />
-              Sign In
-            </Nav.Link>
-          )}
-          {!loading && session && (
-            <>
-              {process.env.NODE_ENV === "development" && (
-                <>
-                  <Link href="/dashboard" passHref>
-                    <Nav.Link>
-                      Dashboard{" "}
-                      <Badge variant="danger" className="ml-1">
-                        BETA
-                      </Badge>
-                    </Nav.Link>
-                  </Link>
-                </>
-              )}
-              <Navbar.Text as="div" className="user-logged-in">
-                <img
-                  src={
-                    session.user.image !== ""
-                      ? session.user.image
-                      : "https://upload.wikimedia.org/wikipedia/commons/9/90/Discord-512.webp"
-                  }
-                  alt={session.user.name}
-                  className="user-image mr-2"
-                />
-                <div className="user-details">
-                  <div className="user-name">
-                    {session.user.name}
-                    {session.user.verified && (
-                      <FontAwesomeIcon className="ml-2" icon={faCheckCircle} />
-                    )}
-                  </div>
-                  <small className="user-signout" onClick={signOut}>
-                    Sign Out
-                  </small>
-                </div>
-              </Navbar.Text>
-            </>
-          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
