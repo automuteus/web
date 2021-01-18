c04ab9c7b361f64c7b83c80c243d54c67483e952
Neil Sparks <neil.sparks.95@gmail.com>
2021-01-17 21:16:14 -0700

    Remove holiday assets

--- components/footer.js
+++ components/footer.js
@@ -1,19 +1,13 @@
 import React from "react";
-import dynamic from "next/dynamic";
-import { faSnowflake } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import EffectToggle from "./effect-toggle";
 
 import styles from "./footer.module.css";
 
-const Snowfall = dynamic(() => import("react-snowfall"), { ssr: false });
 
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      snowActive: true,
+      snowActive: false,
     };
 
     this.onToggle = this.onToggle.bind(this);
@@ -27,17 +21,9 @@ export default class Footer extends React.Component {
 
   render() {
     return (
-      <footer className={`${styles.footer} ${styles.muted_snow} text-right`}>
-        {this.state.snowActive && <Snowfall snowflakeCount={75} />}
-
-        <EffectToggle
-          name="snow-switch"
-          init={this.snowActive}
-          tooltip="Toggle snow"
-          toggle={this.onToggle}
-          label={<FontAwesomeIcon icon={faSnowflake} />}
-        />
-      </footer>
+      <footer
+        className={`${styles.footer} ${styles.muted_snow} text-right`}
+      ></footer>
     );
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import Layout from "../components/layout";
 import ServerStats from "../components/server-stats";
 import Link from "next/link";
 
+const amus_crewmate = "/assets/img/svg/amus_crewmate_robo.svg";
 export default class App extends React.Component {
   render() {
     return (
@@ -17,25 +18,46 @@ export default class App extends React.Component {
         <Head>
           {/* HTML Meta Tags */}
           <title>AutoMuteUs</title>
-          <meta name="description" content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!" />
+          <meta
+            name="description"
+            content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
+          />
           <meta name="theme-color" content="#7289DA" />
 
           {/* Google / Search Engine Tags */}
           <meta itemProp="name" content="AutoMuteUs" />
-          <meta itemProp="description" content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!" />
-          <meta itemProp="image" content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png" />
+          <meta
+            itemProp="description"
+            content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
+          />
+          <meta
+            itemProp="image"
+            content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png"
+          />
 
           {/* Discord/Facebook Meta Tags */}
           <meta property="og:url" content="https://automute.us" />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="AutoMuteUs" />
-          <meta property="og:description" content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!" />
-          <meta property="og:image" content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png" />
+          <meta
+            property="og:description"
+            content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
+          />
+          <meta
+            property="og:image"
+            content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png"
+          />
 
           {/* Twitter Meta Tags */}
           <meta name="twitter:title" content="AutoMuteUs" />
-          <meta name="twitter:description" content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!" />
-          <meta name="twitter:image" content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png" />
+          <meta
+            name="twitter:description"
+            content="AutoMuteUs is a Discord Bot that collects Among Us game data to automatically mute/unmute players during games!"
+          />
+          <meta
+            name="twitter:image"
+            content="http://raw.githubusercontent.com/automuteus/react-web/main/public/assets/img/logo_embed.png"
+          />
         </Head>
         <div id="home-text">
           <h2 className="title">Use AutoMuteUs for hands free muting</h2>
@@ -74,7 +96,7 @@ export default class App extends React.Component {
             id="crewmate"
             alt=""
             type="image/svg+xml"
-            data="/assets/img/svg/amus_crewmate_santa.svg"
+            data={amus_crewmate}
             className="floating"
             aria-label="AutoMuteUs"
           ></object>
