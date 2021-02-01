324d6b45855cb702f591e6b4e5e4b5925a7c0f54
Neil Sparks <neil.sparks.95@gmail.com>
2021-01-31 17:15:40 -0700

    remerge dev into itself

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,17 @@
 import React from "react";
+import dynamic from "next/dynamic";
+import EffectToggle from "./effect-toggle";
 
 import styles from "./footer.module.css";
 
+const Effect = dynamic(() => import("react-confetti"), { ssr: false });
 
 export default class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      snowActive: false,
+      effect: this.props.effect,
+      effectActive: this.props.effectActive && this.props.effect,
     };
 
     this.onToggle = this.onToggle.bind(this);
@@ -15,15 +19,33 @@ export default class Footer extends React.Component {
 
   onToggle(e) {
     this.setState({
-      snowActive: e.target.checked,
+      effectActive: e.target.checked,
     });
   }
-
   render() {
     return (
-      <footer
-        className={`${styles.footer} ${styles.muted_snow} text-right`}
-      ></footer>
+      <footer className={`${styles.footer} ${styles.muted_effect} text-right`}>
+        {this.state.effectActive && (
+          <Effect
+            numberOfPieces={75}
+            opacity={0.65}
+            gravity={0.05}
+            initialVelocityX={5}
+            initialVelocityY={20}
+            run={this.state.effectActive}
+          />
+        )}
+
+        {this.state.effect && (
+          <EffectToggle
+            name="effect-switch"
+            init={this.state.effectActive}
+            tooltip="Toggle confetti"
+            toggle={this.onToggle}
+            label={"🎉"}
+          />
+        )}
+      </footer>
     );
   }
 }
