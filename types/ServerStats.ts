6bc6814adf21bf055215a1ad02bd8a6e9fcb5933
Neil Sparks <neil.sparks.95@gmail.com>
2022-02-19 00:08:12 -0700

    refactor: add index page

---
+++ types/ServerStats.ts
@@ -0,0 +1,9 @@
+export interface ServerStats {
+    activeConnections: number;
+    activeGames: number;
+    commit: string;
+    totalGames: number;
+    totalGuilds: number;
+    totalUsers: number;
+    version: string;
+}
